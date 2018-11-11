fd69e57fbb52d6df5d95a660569c1bc7e3a67a8e
sojurner <pykim0591@gmail.com>
2018-11-11 16:13:24 -0700

    Add Navigation components from bootStrap

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './Navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <Navbar default collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">
+              <img
+                height="80"
+                width="250"
+                src={require(`../../assets/Images/Paul-kim-logo.png`)}
+              />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Carousel indicators={false} controls={false}>
+            <Carousel.Item>
+              <h1 className="quote">
+                Great minds discuss <strong>Ideas</strong>
+              </h1>
+            </Carousel.Item>
+            <Carousel.Item>
+              <h1 className="quote">
+                Average minds discuss <strong>Events</strong>
+              </h1>
+            </Carousel.Item>
+            <Carousel.Item>
+              <h1 className="quote">
+                Small minds discuss <strong>People</strong>
+              </h1>
+            </Carousel.Item>
+          </Carousel>
+        </Navbar.Collapse>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem
+              className="projects-tab"
+              eventKey={1}
+              componentClass={Link}
+              href="/"
+              to="/"
+            >
+              Home
+            </NavItem>
+            <NavItem
+              className="projects-tab"
+              eventKey={2}
+              componentClass={Link}
+              href="/projects"
+              to="/projects"
+            >
+              Projects
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
