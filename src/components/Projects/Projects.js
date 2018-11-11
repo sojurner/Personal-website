93c138ce3deae495e20136491e02c7fe2137696c
sojurner <pykim0591@gmail.com>
2018-11-11 16:13:56 -0700

    Create routes for Project types

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Switch, NavLink, Route } from 'react-router-dom';
+import ProjectContent from '../ProjectContent/ProjectContent';
+
+import './Projects.css';
+
+export default class Projects extends Component {
+  state = {
+    routes: ['Redux', 'React', 'JavaScript', 'Compositions'],
+    description: ['React/Redux', 'React', 'Vanilla JavaScript', 'Html/Css']
+  };
+
+  registerNavLinks = () => {
+    return this.state.routes.map((route, index) => {
+      return (
+        <NavLink className={`nav-link ${route}`} to={`/projects/${route}`}>
+          {this.state.description[index]}
+        </NavLink>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <section className="project-navigation">
+        {this.registerNavLinks()}
+        <Route
+          exact
+          path={`/projects/React`}
+          render={({ match }) => {
+            const { path } = match;
+            const chosenPath = path.slice(10);
+            return <ProjectContent type={chosenPath} />;
+          }}
+        />
+        <Route
+          exact
+          path={`/projects/Redux`}
+          render={({ match }) => {
+            const { path } = match;
+            const chosenPath = path.slice(10);
+
+            return <ProjectContent type={chosenPath} />;
+          }}
+        />
+
+        <Route
+          exact
+          path={`/projects/JavaScript`}
+          render={({ match }) => {
+            const { path } = match;
+            const chosenPath = path.slice(10);
+
+            return <ProjectContent type={chosenPath} />;
+          }}
+        />
+        <Route
+          exact
+          path={`/projects/Compositions`}
+          render={({ match }) => {
+            const { path } = match;
+            const chosenPath = path.slice(10);
+
+            return <ProjectContent type={chosenPath} />;
+          }}
+        />
+      </section>
+    );
+  }
+}
