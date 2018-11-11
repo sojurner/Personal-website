2f4f47c6a0a3c83d590c8c31865e5c117bc4cf3b
sojurner <pykim0591@gmail.com>
2018-11-11 16:14:27 -0700

    Break out SkillSet to individual component

---
+++ src/components/Skillset/Skillset.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { OverlayTrigger } from 'react-bootstrap';
+
+export const Skillset = ({ state, updateDesc, resetDesc }) => {
+  let descriptions = [
+    'React',
+    'Redux',
+    'Enzyme/Jest',
+    'React Router',
+    'Node',
+    'HTML5',
+    'CSS3'
+  ];
+  const people = Object.keys(state).map((description, index) => {
+    const styles = { margin: '0 5px' };
+    return (
+      <OverlayTrigger
+        placement="bottom"
+        overlay={state[description] ? state[description] : null}
+      >
+        <img
+          height="60"
+          style={styles}
+          name={description}
+          onMouseEnter={e => updateDesc(e, descriptions[index])}
+          onMouseLeave={resetDesc}
+          src={require(`../../assets/Images/${description}.png`)}
+        />
+      </OverlayTrigger>
+    );
+  });
+  return <div className="skillset">{people}</div>;
+};
