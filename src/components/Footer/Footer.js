2060eb0ef5b43d1d1642aa40c15814c7484dcfe9
sojurner <pykim0591@gmail.com>
2018-11-11 21:02:38 -0700

    Create Footer component to render weather data

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Footer.css';
+
+export const Footer = ({ weather }) => {
+  console.log(weather);
+  return (
+    <footer className="footer">
+      <img
+        className="weather-icon"
+        src={require(`../../assets/Images/weather/${weather.icon}.png`)}
+      />
+      <div className="weather-info">
+        <div className="high-low">
+          <h1 className="weather weather-high">{weather.high}°F</h1>
+          <h1 className="weather weather-low">{weather.low}°F</h1>
+        </div>
+        <p className="weather weather-summary">{weather.summary}</p>
+      </div>
+      <img
+        className="weather-icon"
+        src={require(`../../assets/Images/weather/${weather.icon}.png`)}
+      />
+    </footer>
+  );
+};
