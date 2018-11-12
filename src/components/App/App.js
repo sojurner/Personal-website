29fccf2e724d63163497fdcf0e84851e7a404d12
sojurner <pykim0591@gmail.com>
2018-11-11 21:02:54 -0700

    Retrieve weather and geolocation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 import { Home } from '../Home/Home';
+import { Footer } from '../Footer/Footer';
+import { getWeather } from '../../utilities/apiCalls';
 import './App.css';
 
 class App extends Component {
+  state = {
+    weather: null
+  };
+  componentDidMount() {
+    this.fetchWeather();
+  }
+
+  fetchWeather = () => {
+    navigator.geolocation.getCurrentPosition(async location => {
+      const { latitude, longitude } = location.coords;
+      const weather = await getWeather(latitude, longitude);
+      await this.setState({ weather });
+    });
+  };
+
   render() {
     return (
       <Router>
         <div>
           <Navigation />
           <Home />
+          {this.state.weather && <Footer weather={this.state.weather} />}
         </div>
       </Router>
     );
