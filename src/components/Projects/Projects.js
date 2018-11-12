9e9fbedf03f3652e1d2cbf02e68fc845add13a8f
sojurner <pykim0591@gmail.com>
2018-11-11 18:59:16 -0700

    Remove uneeded direction

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,13 +6,8 @@ import './Projects.css';
 
 export default class Projects extends Component {
   state = {
-    default: true,
     routes: ['Redux', 'React', 'JavaScript', 'Compositions'],
     description: ['React/Redux', 'React', 'Vanilla JavaScript', 'Html/Css']
-  };
-
-  showDefault = () => {
-    this.setState({ default: false });
   };
 
   registerNavLinks = () => {
@@ -33,11 +28,6 @@ export default class Projects extends Component {
     return (
       <section className="project-navigation">
         {this.registerNavLinks()}
-        {this.state.default && (
-          <h1 className="default-show">
-            Select an option above to view projects
-          </h1>
-        )}
         <Route
           exact
           path={`/projects/React`}
