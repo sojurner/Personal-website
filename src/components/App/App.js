f993a5f19d7fe901cf57a23ab6206f3a8d3e620c
sojurner <pykim0591@gmail.com>
2018-11-15 12:46:45 -0700

    Import and render Ripple component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import Navigation from '../Navigation/Navigation';
 import { Home } from '../Home/Home';
 import { Footer } from '../Footer/Footer';
 import { getWeather } from '../../utilities/apiCalls';
+import Ripples from '../Ripples/Ripples';
 import './App.css';
 
 class App extends Component {
   state = {
     weather: null
   };
+
   componentDidMount() {
     this.fetchWeather();
   }
@@ -26,6 +28,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Ripples />
           <Navigation />
           <Home />
           {this.state.weather && <Footer weather={this.state.weather} />}
