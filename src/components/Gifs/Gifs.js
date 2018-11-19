cbff2ea5885a16be010a25606ac05299b2dd3d7a
sojurner <pykim0591@gmail.com>
2018-11-18 21:48:40 -0700

    Change start and end offset to increments of 15

--- src/components/Gifs/Gifs.js
+++ src/components/Gifs/Gifs.js
@@ -33,11 +33,29 @@ class Gifs extends Component {
     // this.renderGifs();
   };
 
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  searchGifs = async e => {
+    e.preventDefault();
+    const { query, start, end } = this.state;
+    this.setState({ start: 0, end: 6 });
+    const gifs = await calls.getGifs(query);
+    this.setState({
+      gifs: gifs.data,
+      mappedGifs: gifs.data.slice(start, end),
+      loading: false,
+      searchResults: query,
+      query: ''
+    });
+  };
+
   renderGifs = async () => {
     let { start, end, gifs, mappedGifs } = this.state;
     await this.setState({
       start: end + 1,
-      end: end + 6
+      end: end + 14
     });
     await setTimeout(async () => {
       await this.setState({
@@ -47,12 +65,28 @@ class Gifs extends Component {
   };
 
   render() {
+    const { query, mappedGifs, searchResults } = this.state;
     return (
-      <div>
+      <div className="gif-container">
         Gifs
+        <form className="gif-form" onSubmit={e => this.searchGifs(e)}>
+          <input
+            className="gif-input"
+            type="text"
+            value={query}
+            onChange={this.handleChange}
+          />
+          <i className="fab fa-searchengin" />
+        </form>
+        {searchResults && (
+          <h4 className="search-results">
+            Search results for: '<strong>{searchResults}</strong>'
+          </h4>
+        )}
         <div id="scrolling">
           <InfiniteScroll
-            dataLength={this.state.mappedGifs.length}
+            className="infinite-scroll"
+            dataLength={mappedGifs.length}
             next={this.renderGifs}
             hasMore={true}
             loader={<h4>Loading...</h4>}
