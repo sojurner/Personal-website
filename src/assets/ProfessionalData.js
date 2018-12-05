8d860e2e9cac97a4dccc27c91ada15f7fdbdf50c
sojurner <pykim0591@gmail.com>
2018-12-04 16:24:22 -0800

    Add Professionaldata file

---
+++ src/assets/ProfessionalData.js
@@ -0,0 +1,67 @@
+export const data = {
+  skills: {
+    JavaScript: {
+      width: '350',
+      height: '8',
+      x: '95',
+      y: '25'
+    },
+    React: {
+      width: '380',
+      height: '8',
+      x: '95',
+
+      y: '50'
+    },
+    'Vue.JS': {
+      width: '200',
+      height: '8',
+      x: '95',
+
+      y: '75'
+    },
+    'Node.JS': {
+      width: '180',
+      height: '8',
+      x: '95',
+
+      y: '100'
+    },
+    Redux: {
+      width: '380',
+      height: '8',
+      x: '95',
+
+      y: '125'
+    },
+    HTML5: {
+      width: '350',
+      height: '8',
+      x: '95',
+
+      y: '150'
+    },
+    CSS3: {
+      width: '280',
+      height: '8',
+      x: '95',
+
+      y: '175'
+    },
+    Jest: {
+      width: '380',
+      height: '8',
+      x: '95',
+
+      y: '200'
+    },
+    Enzyme: {
+      width: '400',
+      height: '8',
+      x: '95',
+
+      y: '225'
+    }
+  },
+  tools: ['VS Code', 'Git', 'GitHub', 'Heroku', 'PostgreSQL']
+};
