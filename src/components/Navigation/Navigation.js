77e83b422052ed24be4336c3fc0bd1b253868fca
sojurner <pykim0591@gmail.com>
2018-12-18 19:17:52 -0700

    Remove Bootstrap components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, Carousel } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Ripples from '../Ripples/Ripples';
 
 import './Navigation.css';
@@ -40,95 +39,37 @@ class Navigation extends Component {
   render() {
     const { current, previous } = this.state;
     return (
-      <Navbar
-        className={current > previous ? 'nav-bar nav-bar-hide' : 'nav-bar'}
-      >
-        <Ripples />
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">
-              <img
-                alt="Paul Kim Signature"
-                height="60"
-                width="240"
-                src={require(`../../assets/Images/Paul-kim-logo.png`)}
-              />
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Carousel indicators={false} controls={false}>
-            <Carousel.Item>
-              <h1 className="quote_2">
-                <strong>( •.• )ﾉ</strong>
-              </h1>
-            </Carousel.Item>
-            <Carousel.Item>
-              <h1 className="quote_1">
-                <strong>(ﾉ^∇^)ﾉﾟ</strong>
-              </h1>
-            </Carousel.Item>
-            <Carousel.Item>
-              <h1 className="quote_2">
-                <strong>(´◕ ◡ ◕｀)</strong>
-              </h1>
-            </Carousel.Item>
-            <Carousel.Item>
-              <h1 className="quote_3">
-                <strong>( ͡° ͜ʖ ͡°)</strong>
-              </h1>
-            </Carousel.Item>
-            <Carousel.Item>
-              <h1 className="quote_3">
-                <strong>ᵔᴥᵔ</strong>
-              </h1>
-            </Carousel.Item>
-          </Carousel>
-        </Navbar.Collapse>
-        <Navbar.Collapse>
-          <Nav
-            pullRight
-            activeKey={this.state.key}
-            onSelect={k => this.handleSelect(k)}
-          >
-            <NavItem
-              className="projects-tab"
-              eventKey={1}
-              componentClass={Link}
-              href="/"
-              to="/"
-            >
-              Home
-            </NavItem>
-            <NavItem
-              className="projects-tab"
-              eventKey={2}
-              componentClass={Link}
-              href="/#projects/React"
-              to="/projects/React"
-            >
-              Projects
-            </NavItem>
-            <NavItem
-              className="projects-tab"
-              href="/#resume"
-              to="/resume"
-              eventKey={3}
-            >
-              Resume
-            </NavItem>
-            {/* <NavItem
+      <header className={current > previous ? 'nav-header-hide' : 'nav-header'}>
+        {/* <Ripples /> */}
+        <img
+          alt="Paul Kim Signature"
+          height="60"
+          width="240"
+          src={require(`../../assets/Images/Paul-kim-logo.png`)}
+        />
+        <nav className="nav-links">
+          <NavLink className="projects-tab" exact to="/" to="/">
+            <i className="fas fa-home" />
+            Home
+          </NavLink>
+          <NavLink className="projects-tab" to="/projects/React">
+            <i className="fas fa-project-diagram" />
+            Projects
+          </NavLink>
+          <NavLink className="projects-tab" to="/resume">
+            <i className="fas fa-scroll" />
+            Resume
+          </NavLink>
+        </nav>
+        {/* <NavLink
               className="projects-tab"
               href="/#gifs"
               to="/gifs"
               eventKey={4}
             >
               GIFS
-            </NavItem> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+            </NavLink> */}
+      </header>
     );
   }
 }
