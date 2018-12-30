245065d9f5f6f9eb0875c3bc2feee83c19a63216
sojurner <pykim0591@gmail.com>
2018-12-30 10:17:25 -0700

    Implement hover detail show

--- src/components/ExperienceCard/ExperienceCard.js
+++ src/components/ExperienceCard/ExperienceCard.js
@@ -3,16 +3,23 @@ import { experienceInfo } from '../../assets/ProfessionalData';
 
 import './ExperienceCard.css';
 
-export const ExperienceCard = () => {
+export const ExperienceCard = ({ detailDisplay, handleHover }) => {
+  console.log(detailDisplay);
   const experienceCard = experienceInfo.map(item => {
     return (
-      <li>
+      <li
+        onMouseEnter={handleHover.bind(null, item.career)}
+        onMouseLeave={handleHover.bind(null, '')}
+      >
         <span />
         <p className="jumbo-experience-job-title">{item.career}</p>
         <section className="jumbo-experience-time-period">
           <p className="jumbo-experience-time-end">{item.end}</p>
           <p className="jumbo-experience-time-start">{item.start} </p>
         </section>
+        {detailDisplay === item.career && (
+          <section className="detail-popup">{item.details}</section>
+        )}
       </li>
     );
   });
