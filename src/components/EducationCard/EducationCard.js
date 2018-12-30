e012d7cfab769d5bbcef9c216d5272b2924d0a07
sojurner <pykim0591@gmail.com>
2018-12-30 10:17:17 -0700

    Implement hover detail show

--- src/components/EducationCard/EducationCard.js
+++ src/components/EducationCard/EducationCard.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { educationInfo } from '../../assets/ProfessionalData';
 import './EducationCard.css';
 
-export const EducationCard = () => {
+export const EducationCard = ({ detailDisplay, handleHover }) => {
   const educationCard = educationInfo.map(item => {
     return (
-      <li>
+      <li
+        onMouseEnter={handleHover.bind(null, item.institution)}
+        onMouseLeave={handleHover.bind(null, '')}
+      >
         <span />
         <p className="jumbo-education-title">{item.institution}</p>
         <section className="jumbo-education-time-period">
           <p className="jumbo-education-time-end">{item.end}</p>
           <p className="jumbo-education-time-start">{item.start}</p>
         </section>
+        {detailDisplay === item.institution && (
+          <section className="detail-popup">{item.details}</section>
+        )}
       </li>
     );
   });
