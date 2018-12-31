7c754fc39c555726d472652caa7e604154307a9d
sojurner <pykim0591@gmail.com>
2018-12-30 17:16:16 -0700

    Create audio component

---
+++ src/components/Audio/Audio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import AudioPlayer from 'react-h5-audio-player';
+import { withState } from 'recompose';
+
+const Audio = ({ handlePlay, displayAudio }) => {
+  return (
+    <AudioPlayer
+      className={!displayAudio ? 'audio-hide' : 'audio-show'}
+      src={require(`../../assets/music/Ocean-17.mp3`)}
+      controls={true}
+      onPlay={() => handlePlay(!displayAudio)}
+      onPause={() => handlePlay(!displayAudio)}
+    />
+  );
+};
+
+export default withState('displayAudio', 'handlePlay', false)(Audio);
