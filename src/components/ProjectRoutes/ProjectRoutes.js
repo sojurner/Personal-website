e74676b415818cbe7a7af6da61fb0ad4ac0e823c
sojurner <pykim0591@gmail.com>
2018-12-30 17:21:36 -0700

    create project route component

---
+++ src/components/ProjectRoutes/ProjectRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import ProjectContent from '../ProjectContent/ProjectContent';
+import { allProjects } from '../../assets/ProjectData';
+
+const ProjectRoutes = ({ routes }) => {
+  return routes.map((route, index) => {
+    return (
+      <Route
+        key={`route-${index}`}
+        exact
+        path={`/projects/${route}`}
+        render={({ match }) => {
+          const { path } = match;
+          const chosenPath = path.slice(10);
+          const projects = allProjects.filter(
+            proj => proj.projectType === chosenPath
+          );
+          return <ProjectContent projects={projects} />;
+        }}
+      />
+    );
+  });
+};
+
+export default ProjectRoutes;
