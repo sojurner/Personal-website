2d8a2ae53d041102fe0142c1fbf24c6fa909f42c
sojurner <pykim0591@gmail.com>
2018-12-30 17:19:49 -0700

    Refactor tooltips

--- src/components/JumboMumbo/JumboMumbo.js
+++ src/components/JumboMumbo/JumboMumbo.js
@@ -1,52 +1,18 @@
-import React, { Component } from 'react';
-import { Tooltip } from 'react-bootstrap';
-
-import './JumboMumbo.css';
+import React from 'react';
 import AboutTimeline from '../AboutTimeline/AboutTimeline';
 import MainHeader from '../MainHeader/MainHeader';
 import { BucketList } from '../BucketList/BucketList';
 
-class JumboMumbo extends Component {
-  state = {
-    react: null,
-    redux: null,
-    jest: null,
-    vue: null,
-    node: null,
-    html: null,
-    css: null,
-    git: null
-  };
+import './JumboMumbo.css';
 
-  updateDesc = (e, description) => {
-    e.preventDefault();
-    const tooltip = <Tooltip id={description}>{description}</Tooltip>;
-    this.setState({ [e.target.name]: tooltip });
-  };
-
-  resetDesc = e => {
-    this.setState({
-      [e.target.name]: null
-    });
-  };
-
-  handlePlay = () => {
-    this.setState({ displayAudio: !this.state.displayAudio });
-  };
-
-  render() {
-    return (
-      <section className="main-page-jumbo">
-        <MainHeader
-          skills={this.state}
-          updateDesc={this.updateDesc}
-          resetDesc={this.resetDesc}
-        />
-        <AboutTimeline />
-        <BucketList />
-      </section>
-    );
-  }
-}
+const JumboMumbo = () => {
+  return (
+    <section className="main-page-jumbo">
+      <MainHeader />
+      <AboutTimeline />
+      <BucketList />
+    </section>
+  );
+};
 
 export default JumboMumbo;
