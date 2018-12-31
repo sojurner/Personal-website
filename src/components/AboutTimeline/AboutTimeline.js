369571e7c067b3b20d796470b532c848dc11335b
sojurner <pykim0591@gmail.com>
2018-12-30 17:15:54 -0700

    Refactor Timeline

--- src/components/AboutTimeline/AboutTimeline.js
+++ src/components/AboutTimeline/AboutTimeline.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import './AboutTimeline.css';
-import { ExperienceCard } from '../ExperienceCard/ExperienceCard';
-import { EducationCard } from '../EducationCard/EducationCard';
+import ExperienceCard from '../ExperienceCard/ExperienceCard';
+import EducationCard from '../EducationCard/EducationCard';
 
-class AboutTimeline extends React.Component {
-  state = {
-    detailDisplay: ''
-  };
-
-  handleHover = detailDisplay => {
-    this.setState({ detailDisplay });
-  };
-
-  render() {
-    const { detailDisplay } = this.state;
-    return (
-      <section className="jumbo-experience-education">
-        <section className="resume-experience-education">
-          <ExperienceCard
-            detailDisplay={detailDisplay}
-            handleHover={this.handleHover}
-          />
-          <EducationCard
-            detailDisplay={detailDisplay}
-            handleHover={this.handleHover}
-          />
-        </section>
+export default () => {
+  return (
+    <section className="jumbo-experience-education">
+      <section className="resume-experience-education">
+        <ExperienceCard />
+        <EducationCard />
       </section>
-    );
-  }
-}
-
-export default AboutTimeline;
+    </section>
+  );
+};
