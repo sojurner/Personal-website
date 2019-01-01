7e3963f3f47c3386f57ee79d4f71c05cd65cc793
sojurner <pykim0591@gmail.com>
2018-12-31 18:08:41 -0700

    Move coverflow and proj img to separate component

--- src/components/ProjectContent/ProjectContent.js
+++ src/components/ProjectContent/ProjectContent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Image } from 'react-bootstrap';
-import Coverflow from 'react-coverflow';
+// import Coverflow from 'react-coverflow';
 
 import ProjectModal from '../ProjectModal/ProjectModal';
 import './ProjectContent.css';
+import ProjectCoverflow from '../ProjectCoverflow/ProjectCoverflow';
 class ProjectContent extends Component {
   constructor() {
     super();
@@ -22,47 +22,15 @@ class ProjectContent extends Component {
     this.setState({ show: true, selectedProject });
   };
 
-  handleHover = title => {
-    this.setState({ title });
-  };
-
-  renderProjectImageCards = () => {
-    return this.props.projects.map((project, index) => {
-      return (
-        <Image
-          key={`project-${index}`}
-          className="project-gifs"
-          onClick={this.handleShow.bind(null, project)}
-          onMouseEnter={this.handleHover.bind(null, project.title)}
-          onMouseLeave={this.handleHover.bind(null, '')}
-          enableHeading={true}
-          style={{ display: 'block', width: '100%' }}
-          alt={
-            project.title === this.state.title ? 'Click to view' : project.title
-          }
-          src={require(`../../assets/Images/project-ss/${project.title}.png`)}
-        />
-      );
-    });
-  };
-
   render() {
     const { show, selectedProject } = this.state;
 
     return (
       <section className="project-content-container">
-        <Coverflow
-          width="900"
-          height="800"
-          displayQuantityOfSide={1}
-          infiniteScroll={true}
-          navigation={true}
-          enableScroll={false}
-          clickable={true}
-          active={0}
-        >
-          {this.renderProjectImageCards()}
-        </Coverflow>
+        <ProjectCoverflow
+          handleShow={this.handleShow}
+          projects={this.props.projects}
+        />
         {show && (
           <ProjectModal
             show={show}
