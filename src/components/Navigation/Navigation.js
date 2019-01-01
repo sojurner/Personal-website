865a5de0733e6e9c5aedb572c418df487c62ae92
sojurner <pykim0591@gmail.com>
2018-12-31 17:41:41 -0700

    toggle classes of nav-link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -37,14 +37,17 @@ class Navigation extends Component {
   render() {
     const { current, previous } = this.state;
     return (
-      <header className={current > previous ? 'nav-header-hide' : 'nav-header'}>
+      <header className="nav-header-show">
         <img
           alt="Paul Kim Signature"
           height="60"
+          className={current > previous ? 'logo-hide' : 'logo-show'}
           width="240"
           src={require(`../../assets/Images/Paul-kim-logo.png`)}
         />
-        <nav className="nav-links">
+        <nav
+          className={current > previous ? 'nav-links-hide' : 'nav-links-show'}
+        >
           <NavLink className="projects-tab" exact to="/">
             <i className="fas fa-home" />
             Home
