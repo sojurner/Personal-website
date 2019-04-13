5cefbb13451b8521c9927ef3e794241e9037a008
Paul Kim <paul.kim@catalystrms.com>
2019-04-12 21:33:39 -0600

    Conditional classes

--- src/components/MainLanding/MainLanding.js
+++ src/components/MainLanding/MainLanding.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './MainLanding.css';
 
-const MainLanding = ({ mainRef }) => {
+const MainLanding = ({ mainRef, titleClass }) => {
   return (
     <div ref={mainRef} className="header-job-family">
       <header className="job-location">
-        <h1 className="job-title"> Software Developer</h1>
+        <h1 className={!titleClass ? 'job-title' : 'job-title job-title-show'}>
+          {' '}
+          Software Developer
+        </h1>
         <section className="audio-location">
           <h2 className="location-title"> Denver, CO</h2>
         </section>
