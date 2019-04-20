905b43dcc2002713e097c6ce168960b2263dc356
Paul Kim <paul.kim@catalystrms.com>
2019-04-20 12:04:32 -0600

    Move classname change to container

--- src/components/MainLanding/MainLanding.js
+++ src/components/MainLanding/MainLanding.js
@@ -5,11 +5,12 @@ import Skillset from '../Skillset/Skillset';
 const MainLanding = ({ mainRef, titleClass }) => {
   return (
     <div ref={mainRef} className="header-job-family">
-      <header className="job-location">
-        <h1 className={!titleClass ? 'job-title' : 'job-title job-title-show'}>
-          {' '}
-          Software Developer
-        </h1>
+      <header
+        className={
+          !titleClass ? 'landing-content-hide' : 'landing-content-show'
+        }
+      >
+        <h1 className={'job-title'}> Software Developer</h1>
         <section className="audio-location">
           <Skillset />
         </section>
