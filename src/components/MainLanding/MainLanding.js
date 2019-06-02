81a0361878463c64eebc37fae53c83aa03a4134e
Paul Kim <paul.kim@catalystrms.com>
2019-06-01 19:00:36 -0600

    Add location info

--- src/components/MainLanding/MainLanding.js
+++ src/components/MainLanding/MainLanding.js
@@ -10,6 +10,7 @@ const MainLanding = ({ mainRef, titleClass }) => {
           !titleClass ? 'landing-content-hide' : 'landing-content-show'
         }
       >
+        <h5 className={'residence'}>Denver, CO</h5>
         <h1 className={'job-title'}> Software Developer</h1>
         <section className="audio-location">
           <Skillset />
