dc8ea1a1f27c99753e9cc6ab99650e9b2d6d0ead
Paul Kim <paul.kim@catalystrms.com>
2019-06-06 19:33:06 -0600

    increase bar dimensions

--- src/components/SkillBars/SkillBars.js
+++ src/components/SkillBars/SkillBars.js
@@ -15,15 +15,16 @@ export const SkillBars = ({
         className={`bar bar${index}`}
         onMouseEnter={handleTooltip.bind(null, key)}
         onMouseLeave={handleTooltip.bind(null, '')}
+        height={5}
       >
         <rect
           className="lead-bar"
           width={500}
-          height={3}
+          height={4}
           rx="3"
           ry="1"
           x={skills[key].x}
-          y={skills[key].y - 0.8}
+          y={skills[key].y * 1.1 - 0.8}
         />
         <rect
           className={delayBars ? 'skill-bar-hide' : 'skill-bar-show'}
@@ -33,12 +34,12 @@ export const SkillBars = ({
           width={skills[key].width}
           height={skills[key].height}
           x={skills[key].x}
-          y={skills[key].y - 3}
+          y={skills[key].y * 1.1 - 3}
         />
-        <text x="0" y={30 + 25 * index} className="bar-text">
+        <text x="0" y={31 + 28 * index} className="bar-text">
           {key}
         </text>
-        <text
+        {/* <text
           className={
             displayTooltip === key ? 'tooltip-text' : 'tooltip-text-hide'
           }
@@ -47,7 +48,7 @@ export const SkillBars = ({
         >
           {skills[key].tooltip}
         </text>
-        )}
+        )} */}
       </g>
     );
   });
